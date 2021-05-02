0e7458a43514bf1db80db0ceb693ac9725332296
qiang zhang <qiang.zhang@forcepoint.com>
2021-05-01 23:27:48 -0400

    initial artifacts as of May 1st

---
+++ OnlineTutor/Views/Home/privacy.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OnlineTutor.Views.Home
+{
+    public class privacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
